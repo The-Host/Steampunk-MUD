9493cdb879761645517ecb09a28a9967b91e4933
Benjamin Mallin <b.m.mallin@gmail.com>
2014-06-18 13:27:19 -0400

    PEP-8 compliance for client.py.

--- client/client.py
+++ client/client.py
@@ -1,18 +1,26 @@
-#First run server, then client.
-#When client is ran, server gets a message from it
-#and sends a message back.
-#After client gets the server message, the client closes.
+# First run server, then client. When client is ran, server gets a
+# message from it and sends a message back.After client gets the
+# server message, the client closes.
 
 import socket
 
-data = "Hello from the client!" #DAta to be sent to server
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create socket object
+if __name__ == '__main__':
+    # Data to be sent to server
+    data = "Hello from the client!"
 
-sock.connect(('127.0.0.1', 4343)) #must be the address and port of server
-sock.send(bytes(data, 'UTF-8')) #sending data in form of bytes
+    # create socket object
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-result = sock.recv(1024).decode('UTF-8') #data that the server sent to client
-print(result)
+    # must be the address and port of server
+    sock.connect(('127.0.0.1', 4343))
 
-sock.close() #end socket object
+    # sending data in form of bytes
+    sock.send(bytes(data, 'UTF-8'))
+
+    # data that the server sent to client
+    result = sock.recv(1024).decode('UTF-8')
+    print(result)
+
+    # end socket object
+    sock.close()
